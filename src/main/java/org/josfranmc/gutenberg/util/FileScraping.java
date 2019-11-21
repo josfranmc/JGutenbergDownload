ecc2dcf4675e1f9e07f7a4511dd23895a639bc1d
josfranmc <josfranmc@gmail.com>
2019-11-21 13:38:39 +0100

    Incluir notas de licencia

--- src/main/java/org/josfranmc/gutenberg/util/FileScraping.java
+++ src/main/java/org/josfranmc/gutenberg/util/FileScraping.java
@@ -1,3 +1,24 @@
+/*
+ *  Copyright (C) 2018-2019 Jose Francisco Mena Ceca <josfranmc@gmail.com>
+ *
+ *  This file is part of JGutenbergDownload.
+ *
+ *  JGutenbergDownload is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JGutenbergDownload is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with JGutenbergDownload.  If not, see <https://www.gnu.org/licenses/>.
+ *    
+ *  This file includes software developed at
+ *  The Apache Software Foundation (http://www.apache.org/). 
+ */ 
 package org.josfranmc.gutenberg.util;
 
 import java.io.IOException;
